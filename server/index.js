4e4108d1d51600b3a4ec3220a379290b4b5964d1
aedelfrid <almck51@gmail.com>
2024-05-06 18:14:59 -0300

    added fetch to reddit servers, starting working out pagination

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
-const express = require("express")
+const express = require("express");
+const fetch = require('node-fetch');
+
+let subreddit = "cmhoc";
+let page = 1;
 
 // server
-    // express
-
-
+// express
 // database
-    // mongo?
+// mongo?
 // reddit api
-    // fetch
+// fetch
+
+async function getRedditPosts(subreddit, page) {
+    const limit = (page + 1) * 10;
+    const count = limit - 10; 
+
+    const response = await fetch(`https://api.reddit.com/r/${subreddit}/new?count=${count}?limit=${limit}`)
+    const posts = await response.json()
+
+    console.log(posts)
+}
+
+async function getDaily(params) {
+    
+}
+
+try {
+    getRedditPosts(subreddit, page)
+    
+} catch {
+    console.log("fetch failed")
+}
+
