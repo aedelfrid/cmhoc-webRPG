981a84a2a77e49da4859c89250daa8b71b17e15d
aedelfrid <almck51@gmail.com>
2024-05-08 20:00:30 -0300

    added backend support for login

---
+++ server/controllers/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const User = require("../../models/User");
+
+router.post("/login", async (req, res) => {
+    // get user from db
+    const userData = User.findOne( { username: req.body.username } );
+
+    // if no user exists, return error
+    if (!userData) {
+        throw new Error('Incorrect username!');
+    };
+
+    // check password against stored
+    const correctPw = await User.isCorrectPassword(req.body.password);
+
+    if (!correctPw) {
+        throw new Error('Incorrect password!');
+    }
+
+    // return a JWT token
+
+    const token = signToken(userData);
+    return { token, userData };
+});
--- server/index.js
+++ server/index.js
@@ -1,25 +1,23 @@
 const express = require("express")
 require("dotenv").config();
 
-const api = require('./routes/index.js');
+const api = require('./controllers/index.js');
 
 const app = express();
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3002;
 
-const startServer = async () => {
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
-    app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
-    app.use('/api', api);
+app.use('/api', api);
 
-    app.get('/', (req, res) =>
-        res.sendFile(path.join(__dirname, '../client/index.html'))
-    );
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+);
 
-    app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}`)
-    })
-};
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
