9772d7fd262b42772d163e7067e4f4d53149ee69
aedelfrid <almck51@gmail.com>
2024-05-09 15:55:54 -0300

    changed to auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt_decode = require('jwt-decode');
 const secret = "397A2859675EC4E447E66917F8BB8"
 
 module.exports = {
-    authMiddleware: function (req, res, next) {
+    authMiddleware: function (req, res) {
         let token = req.body.token || req.query.token || req.headers.authorization;
 
         if (req.headers.authorization) {
